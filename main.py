493616e3a95c3c675c72d92a923e56bbde38430a
Gustavo Pfleger Rebelo <gustavo.pr2005@aluno.ifsc.edu.br>
2025-03-06 21:14:24 -0300

    iniciando a implementação de rects, ainda no projeto de teste

--- main.py
+++ main.py
@@ -8,25 +8,31 @@ pygame.display.set_caption("Flappy Bird")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("assets/Pixeltype.ttf", 50)
 
-#surfaces
-sky_surface = pygame.image.load("assets./Sky.png").convert()
-ground_surface = pygame.image.load("assets./ground.png").convert()
-text_surface = test_font.render("Hello World!!", False, "black")
-snail_surface = pygame.image.load("assets./snail./snail1.png").convert_alpha()
-snail_x = 800
+#surfs
+sky_surf = pygame.image.load("assets./Sky.png").convert()
+ground_surf = pygame.image.load("assets./ground.png").convert()
+text_surf = test_font.render("Hello World!!", False, "black")
+
+snail_surf = pygame.image.load("assets./snail./snail1.png").convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom = (800, 300))
+
+player_surf = pygame.image.load("assets./player./player_walk_1.png").convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80, 300))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (20, 20))
-    screen.blit(snail_surface, (snail_x, 265))
-    snail_x -= 4
-    if snail_x <= -100:
-        snail_x = 800
+    screen.blit(sky_surf, (0, 0))
+    screen.blit(ground_surf, (0, 300))
+    screen.blit(text_surf, (20, 20))
+    screen.blit(snail_surf, snail_rect)
+    snail_rect.left -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+    screen.blit(player_surf, player_rect)
     pygame.display.update() 
     clock.tick(60)
- 
+
