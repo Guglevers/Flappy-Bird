c3c57e921347cf69d30ef987a5236700f10eb249
Gustavo Pfleger Rebelo <gustavo.pr2005@aluno.ifsc.edu.br>
2025-03-15 01:40:53 -0300

    aprendendo sobre o metodo draw e colisões

--- main.py
+++ main.py
@@ -11,13 +11,17 @@ test_font = pygame.font.Font("assets/Pixeltype.ttf", 50)
 #surfs
 sky_surf = pygame.image.load("assets./Sky.png").convert()
 ground_surf = pygame.image.load("assets./ground.png").convert()
+
 text_surf = test_font.render("Hello World!!", False, "black")
+text_rect = text_surf.get_rect(center = (400, 20))
 
 snail_surf = pygame.image.load("assets./snail./snail1.png").convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom = (800, 300))
 
 player_surf = pygame.image.load("assets./player./player_walk_1.png").convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80, 300))
+
+cursor_surf = pygame.image.load("assets./aim.png").convert_alpha()
 
 while True:
     for event in pygame.event.get():
@@ -27,16 +31,15 @@ while True:
 
     screen.blit(sky_surf, (0, 0))
     screen.blit(ground_surf, (0, 300))
-    screen.blit(text_surf, (20, 20))
+    screen.blit(text_surf, text_rect)
+
     screen.blit(snail_surf, snail_rect)
     snail_rect.left -= 4
     if snail_rect.right <= 0:
         snail_rect.left = 800
     screen.blit(player_surf, player_rect)
 
-    if player_rect.colliderect(snail_rect):
-        print("my balls")
-
+    pygame.draw.line(screen, "black", (0, 0), (800, 400), 1)
     pygame.display.update() 
     clock.tick(60)
 
